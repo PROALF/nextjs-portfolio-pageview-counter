1b1c9435827dc3d031960eafba70256e50999a6e
ProALF <140237262+PROALF@users.noreply.github.com>
2023-09-15 17:04:44 +0200

    h

--- app/page.tsx
+++ app/page.tsx
@@ -29,14 +29,14 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				chronark
+				Einfach.Alex
 			</h1>
 
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Alex, I'm building and develope serverless and open source
+					solutions {" "}
 					<Link
 						target="_blank"
 						href="https://upstash.com"
@@ -49,7 +49,7 @@ export default function Home() {
 					and working on{" "}
 					<Link
 						target="_blank"
-						href="https://unkey.dev"
+						href="https://einfachalex.net"
 						className="underline duration-500 hover:text-zinc-300"
 					>
 						unkey.dev
